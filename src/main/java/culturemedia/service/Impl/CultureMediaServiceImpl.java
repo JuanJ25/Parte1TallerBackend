1a3eae65ea22d91580e3de5c1e04cfcae35ab1b2
JuanJ25 <juanpaterninac@gmail.com>
2024-04-17 21:39:50 -0500

    Update for Impl and service package.

---
+++ src/main/java/culturemedia/service/Impl/CultureMediaServiceImpl.java
@@ -0,0 +1,25 @@
+package culturemedia.service.Impl;
+import culturemedia.model.Video;
+import culturemedia.model.VideoReproduction;
+import culturemedia.repository.VideoRepository;
+import culturemedia.repository.ViewsRepository;
+import culturemedia.service.CultureMediaService;
+
+import java.util.List;
+
+public class CultureMediaServiceImpl implements CultureMediaService {
+    private VideoRepository videoRepository;
+    private ViewsRepository viewsRepository;
+    public List<Video> findAll() {
+        return videoRepository.findAll();
+    }
+
+    public Video save(Video video)  {
+        return videoRepository.save(video);
+    }
+
+    public VideoReproduction save(VideoReproduction view)  {
+        return viewsRepository.save(view);
+    }
+
+}
