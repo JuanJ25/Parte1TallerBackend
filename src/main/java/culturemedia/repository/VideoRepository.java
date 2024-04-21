4700999bfeecc2429e57164d9512a5b7ac4f8435
JuanJ25 <juanpaterninac@gmail.com>
2024-04-21 13:02:57 -0500

    New branch part 6.

--- src/main/java/culturemedia/repository/VideoRepository.java
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -6,11 +6,7 @@ import culturemedia.model.Video;
 public interface VideoRepository {
     List<Video> findAll();
     Video save(Video video);
-    List<Video> findByTitle(String title);
-    List<Video> findByDuration(Double fromDuration, Double toDuration);
-
     List<Video> find(String title);
-
     List<Video> find(Double fromDuration, Double toDuration);
 }
 
